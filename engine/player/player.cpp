9b333d157888ac3ca0d186e9e45514471b0b47b1
nyterage <nyterage@gmail.com>
2025-03-12 08:54:22 -0700

    [Player] Only try to check for shadowmeld's buff if the player is a night elf

--- engine/player/player.cpp
+++ engine/player/player.cpp
@@ -6737,7 +6737,7 @@ void player_t::enter_combat()
   for ( size_t i = 0; i < callbacks_on_combat_state.size(); ++i )
     callbacks_on_combat_state[ i ]( this, in_combat );
 
-  if ( buffs.shadowmeld->check() )
+  if ( race == RACE_NIGHT_ELF && buffs.shadowmeld->check() )
     buffs.shadowmeld->expire();
 }
 
