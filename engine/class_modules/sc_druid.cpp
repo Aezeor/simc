69738452d4fd0a51bd1ea6755d6a0b3ff8fb0cd9
gastank <42421688+gastank@users.noreply.github.com>
2025-07-28 08:28:41 -0700

    [Guardian] ravage echo not affected by tooth and claw & vicious cycle

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -15309,9 +15309,13 @@ void druid_t::parse_action_effects( action_t* action )
   _a->parse_effects( spec.fury_of_nature, effect_mask_t( true ).disable( 1 ), talent.lunar_calling );
   _a->parse_effects( buff.gory_fur, EXPIRE_BUFF );
   _a->parse_effects( buff.rage_of_the_sleeper );
-  _a->parse_effects( buff.tooth_and_claw );
   _a->parse_effects( buff.vicious_cycle_mangle, USE_DEFAULT, EXPIRE_BUFF );
-  _a->parse_effects( buff.vicious_cycle_maul, USE_DEFAULT, EXPIRE_BUFF );
+
+  if ( !dynamic_cast<druid_action_data_t*>( action )->has_flag( flag_e::TWW3SET ) )
+  {
+    _a->parse_effects( buff.tooth_and_claw );
+    _a->parse_effects( buff.vicious_cycle_maul, USE_DEFAULT, EXPIRE_BUFF );
+  }
 
   _a->parse_effects( buff.guardians_tenacity );
   // effects#5 and #6 are ignored regardless of lunar calling
