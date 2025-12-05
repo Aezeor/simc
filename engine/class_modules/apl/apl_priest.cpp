3122e1632f9a6a9816bdef894f3da2062c553808
Publik <sean.peters86@icloud.com>
2025-12-04 20:55:34 -0600

    [Priest] fix default flask name

--- engine/class_modules/apl/apl_priest.cpp
+++ engine/class_modules/apl/apl_priest.cpp
@@ -18,7 +18,7 @@ std::string potion( const player_t* p )
 
 std::string flask( const player_t* p )
 {
-  return ( p->true_level > 80 ) ? "midnight_mastery_flask_2" : "flask_of_alchemical_chaos_3";
+  return ( p->true_level > 80 ) ? "flask_of_the_magisters_2" : "flask_of_alchemical_chaos_3";
 }
 
 std::string food( const player_t* p )
