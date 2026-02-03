86eac78b334384ff217695f1e481522f2242854a
Timothy Seltman <tseltman@verizon.net>
2026-02-02 23:06:49 -0500

    [Monk] Fix Combo Breaker Falllback chance.

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -5705,7 +5705,8 @@ void monk_t::create_buffs()
   buff.combo_breaker = make_buff_fallback( specialization() == MONK_WINDWALKER, this, "combo_breaker",
                                            talent.windwalker.combo_breaker_buff )
                            ->set_trigger_spell( talent.windwalker.combo_breaker )
-                           ->set_chance( talent.windwalker.combo_breaker->effectN( 1 ).percent() );
+                           ->set_chance( talent.windwalker.combo_breaker->ok() ? 1.0 
+                               : talent.windwalker.combo_breaker->effectN( 1 ).percent() );
 
   buff.chi_energy =
       make_buff_fallback( talent.windwalker.jade_ignition->ok(), this, "chi_energy", talent.windwalker.chi_energy_buff )
