2023436ab70cf85b48a02b135d09a541fa90a2ca
Saeldur <5496498+Saeldur@users.noreply.github.com>
2026-02-04 05:10:19 +0000

    [Devourer] Drainrate Adjustments
    [Devourer] Drainrate Formula Adjustment
    * Not perfect - likely something else going on but this is slightly more
      accurate.

--- engine/class_modules/sc_demon_hunter.cpp
+++ engine/class_modules/sc_demon_hunter.cpp
@@ -1169,8 +1169,8 @@ public:
     double soul_fragment_from_shattered_souls_chance = 0.4;
     int entropy_starting_souls                       = -1;
     int channel_tick_cutoff_benefit                  = 2;
-    double void_metamorphosis_initial_drain          = 8.0;
-    double void_metamorphosis_drain_per_stack        = 0.025;
+    double void_metamorphosis_initial_drain          = 7.5;
+    double void_metamorphosis_exp_stack_mul          = 0.0058;
   } options;
 
   demon_hunter_t( sim_t* sim, util::string_view name, race_e r );
@@ -1344,8 +1344,8 @@ public:
     event_t* next_drain_event;
     int drain_stacks;
     demon_hunter_t* actor;
-    double initial_drain   = 10.0;
-    double drain_per_stack = 0.012;
+    double initial_drain   = 0.0;
+    double exp_stack_mul = 0.0;
 
     fury_state_t( demon_hunter_t* a )
       : next_drain_event( nullptr ),
@@ -1359,13 +1359,11 @@ public:
     void init()
     {
       initial_drain   = p()->options.void_metamorphosis_initial_drain;
-      drain_per_stack = p()->options.void_metamorphosis_drain_per_stack;
+      exp_stack_mul = p()->options.void_metamorphosis_exp_stack_mul;
 
       if ( p()->talent.devourer.soul_glutton.enabled() )
-
       {
-        // This is negative and lowers something to 75% for some reason
-        initial_drain *= 1 - p()->talent.devourer.soul_glutton->effectN( 2 ).percent();
+        initial_drain /= ( 1 + p()->talent.devourer.soul_glutton->effectN( 2 ).percent() );
       }
     }
 
@@ -1389,7 +1387,7 @@ public:
 
     double base_fury_drain_per_second( int stacks ) const
     {
-      return initial_drain + drain_per_stack * stacks;
+      return initial_drain + exp( exp_stack_mul * stacks );
     }
 
     timespan_t base_time_to_next_tick( int stacks ) const
@@ -10166,7 +10164,7 @@ void demon_hunter_t::create_options()
   add_option( opt_int( "channel_tick_cutoff_benefit", options.channel_tick_cutoff_benefit, 0, 10 ) );
 
   add_option( opt_float( "void_metamorphosis_initial_drain", options.void_metamorphosis_initial_drain, 0, 100 ) );
-  add_option( opt_float( "void_metamorphosis_drain_per_stack", options.void_metamorphosis_drain_per_stack, 0, 100 ) );
+  add_option( opt_float( "void_metamorphosis_exp_stack_mul", options.void_metamorphosis_exp_stack_mul, 0, 100 ) );
 }
 
 // demon_hunter_t::create_pet ===============================================
