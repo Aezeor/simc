4bf787148b214f2fcc9daf22215c2b779d9e79b2
Saeldur <5496498+Saeldur@users.noreply.github.com>
2026-04-02 17:33:21 +0100

    [Augmentation] Accidental bag of sand

--- engine/class_modules/sc_evoker.cpp
+++ engine/class_modules/sc_evoker.cpp
@@ -368,7 +368,7 @@ struct simplified_player_t : public player_t
   // Options
   struct options_t
   {
-    int item_level      = 265;
+    int item_level      = 278;
     std::string variant = "default";
   } option;
 
